b6387bac3adfd61e42852e2c2efdc439c5a8c9bf
cadenmr <caden.m.r@cox.net>
2024-02-21 18:45:43 -0700

    update

--- bitmap_converter_small.py
+++ bitmap_converter_small.py
@@ -1,65 +1,14 @@
-import sys
+import os
 from PIL import Image
 
-filename_in = sys.argv[1]
-filename_out = sys.argv[2]  # future use
+# filename_in = sys.argv[1]
+# filename_out = sys.argv[2]  # future use
 
-try:
-    texture = Image.open(filename_in)
-except FileNotFoundError:
-    print('file not found')
-    exit(1)
-    
-if texture.size != (32, 16):
-    print('incorrect image size')
-    exit(1)
-    
-righthand_eye_pixels_arr = []
-lefthand_eye_pixels_arr = []
-center_pixels_arr = []
+filenames_in = os.listdir('input')
 
-# right-hand eye
-even_line = True    # run forwards on even lines, reverse on odd lines
-for x in range(0, 8):
-    
-    if even_line:
-        for y in range(0, 8):
-            righthand_eye_pixels_arr.append(texture.getpixel((x,y))[:3])
-        even_line = False
-        
-    else:
-        for y in range(7, -1, -1):
-            righthand_eye_pixels_arr.append(texture.getpixel((x,y))[:3])
-        even_line = True
-        
-# left-hand eye
-even_line = True    # run forwards on even lines, reverse on odd lines
-for x in range(24, 32):
-    
-    if even_line:
-        for y in range(0, 8):
-            lefthand_eye_pixels_arr.append(texture.getpixel((x,y))[:3])
-        even_line = False
-        
-    else:
-        for y in range(7, -1, -1):
-            lefthand_eye_pixels_arr.append(texture.getpixel((x,y))[:3])
-        even_line = True
-        
-# center
-even_line = True    # run forwards on even lines, reverse on odd lines
-for x in range(0, 32):
-    
-    if even_line:
-        for y in range(8, 16):
-            center_pixels_arr.append(texture.getpixel((x,y))[:3])
-        even_line = False
-        
-    else:
-        for y in range(15, 7, -1):
-            center_pixels_arr.append(texture.getpixel((x,y))[:3])
-        even_line = True
-        
+output_file = open('incl_out.ino', 'w')
+
+pic_counter = 0
 
 def form_data_str(pixels_arr): 
 
@@ -92,12 +41,130 @@ def form_data_str(pixels_arr):
     
     return data_str
 
-rh_eye_str = form_data_str(righthand_eye_pixels_arr)
-lh_eye_str = form_data_str(lefthand_eye_pixels_arr)
-center_data_str = form_data_str(center_pixels_arr)
+for filename in filenames_in:
+    
+    output_file.write('// ' + filename + '\n')
+    
+    filename_in = 'input/' + filename
+    filename_no_extension = filename.split('.')[0]
+    
+    try:
+        texture = Image.open(filename_in)
+    except FileNotFoundError:
+        print('file not found')
+        exit(1)
+    
+    if texture.size == (32, 16):
+            
+        righthand_eye_pixels_arr = []
+        lefthand_eye_pixels_arr = []
+        center_pixels_arr = []
 
-print(rh_eye_str)
-print('\n\n', end=None)
-print(lh_eye_str)
-print('\n\n', end=None)
-print(center_data_str)
+        # right-hand eye
+        even_line = True    # run forwards on even lines, reverse on odd lines
+        for x in range(0, 8):
+            
+            if even_line:
+                for y in range(0, 8):
+                    righthand_eye_pixels_arr.append(texture.getpixel((x,y))[:3])
+                even_line = False
+                
+            else:
+                for y in range(7, -1, -1):
+                    righthand_eye_pixels_arr.append(texture.getpixel((x,y))[:3])
+                even_line = True
+                
+        # left-hand eye
+        even_line = True    # run forwards on even lines, reverse on odd lines
+        for x in range(24, 32):
+            
+            if even_line:
+                for y in range(0, 8):
+                    lefthand_eye_pixels_arr.append(texture.getpixel((x,y))[:3])
+                even_line = False
+                
+            else:
+                for y in range(7, -1, -1):
+                    lefthand_eye_pixels_arr.append(texture.getpixel((x,y))[:3])
+                even_line = True
+                
+        # center
+        even_line = True    # run forwards on even lines, reverse on odd lines
+        for x in range(0, 32):
+            
+            if even_line:
+                for y in range(8, 16):
+                    center_pixels_arr.append(texture.getpixel((x,y))[:3])
+                even_line = False
+                
+            else:
+                for y in range(15, 7, -1):
+                    center_pixels_arr.append(texture.getpixel((x,y))[:3])
+                even_line = True
+                
+        rh_eye_str = form_data_str(righthand_eye_pixels_arr)
+        lh_eye_str = form_data_str(lefthand_eye_pixels_arr)
+        center_data_str = form_data_str(center_pixels_arr)
+
+        output_file.write('// righthand eye\n' + f'uint8_t {filename_no_extension}_rightEye[64][3] = ' + rh_eye_str)
+        output_file.write('\n\n')
+        output_file.write('// lefthand eye\n' + f'uint8_t {filename_no_extension}_leftEye[64][3] = ' + lh_eye_str)
+        output_file.write('\n\n')
+        output_file.write('// center\n' + f'uint8_t {filename_no_extension}_center[256][3] = ' + center_data_str)
+        output_file.write('\n\n')
+
+    elif texture.size == (8, 8):
+        
+        eye_pixels_arr = []
+        
+        # right-hand eye
+        even_line = True    # run forwards on even lines, reverse on odd lines
+        for x in range(0, 8):
+            
+            if even_line:
+                for y in range(0, 8):
+                    eye_pixels_arr.append(texture.getpixel((x,y))[:3])
+                even_line = False
+                
+            else:
+                for y in range(7, -1, -1):
+                    eye_pixels_arr.append(texture.getpixel((x,y))[:3])
+                even_line = True
+                
+        eye_str = form_data_str(eye_pixels_arr)
+                
+        if 'left' in filename:
+            output_file.write('// lefthand eye\n' + f'uint8_t {filename_no_extension}_leftEye[64][3] = ' + eye_str)
+            output_file.write('\n\n')
+            
+        elif 'right' in filename:
+            output_file.write('// righthand eye\n' + f'uint8_t {filename_no_extension}_rightEye[64][3] = ' + eye_str)
+            output_file.write('\n\n')
+            
+        else:
+            print("8x8 with no left/right inidcator, skipping...")      
+    
+    elif texture.size == (32, 8):
+        
+        even_line = True    # run forwards on even lines, reverse on odd lines
+        for x in range(0, 32):
+            
+            if even_line:
+                for y in range(0, 8):
+                    center_pixels_arr.append(texture.getpixel((x,y))[:3])
+                even_line = False
+                
+            else:
+                for y in range(7, -1, -1):
+                    center_pixels_arr.append(texture.getpixel((x,y))[:3])
+                even_line = True
+                
+        center_data_str = form_data_str(center_pixels_arr)
+        output_file.write('// center\n' + f'uint8_t {filename_no_extension}_center[256][3] = ' + center_data_str)
+    
+    pic_counter += 1
+        
+
+print(f'processed {pic_counter} images')
+
+output_file.close()
